7d014a657158d43a7cf936ae9b88430fbdedff63
Wu Louis <louiswu148@gmail.com>
2024-06-02 14:47:15 +0900

    Initial Commit

---
+++ BadIcon/BadIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadIconApp.swift
+//  BadIcon
+//
+//  Created by 刘易斯 on 2024/06/02.
+//
+
+import SwiftUI
+
+@main
+struct BadIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
